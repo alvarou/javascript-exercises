ab00473374a681b3d56f221246905fca0a6f62da
AU <urrutia.alvaro@gmail.com>
2022-07-28 16:02:20 -0500

    Use map to apply a function on each array element

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,7 @@
-const getTheTitles = function() {
+//given an array of objrects, 
+//return an array of the titles of the objects
+const getTheTitles = function(books) {
+    return books.map(book => book.title);
 
 };
 
