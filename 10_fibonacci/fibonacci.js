0fff7cf8a2bde009f70da1fd8bbae4d3d8e3f3c0
AU <urrutia.alvaro@gmail.com>
2022-07-28 15:31:19 -0500

    Add a function to return nth fibonnacci number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+//arrow function that returns the nth fibonacci number
+const fibonacci = (n) => {
+   
+    //convert n to a number
+    n = Number(n);
+    
+    //if n is negative, return "OOPS"
+    if (n < 0) {
+        return "OOPS";
+    }
+    if (n === 0) {
+        return 0;
+    }
+    if (n === 1) {
+        return 1;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
 // Do not edit below this line
