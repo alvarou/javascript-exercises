77e62de7b6b2d4452fa5249cf5de0d803c45c638
AU <urrutia.alvaro@gmail.com>
2022-07-28 06:59:10 -0500

    Reverse a string using a for loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -5,8 +5,14 @@ const reverseString = function(string) {
   }
   
   //using recursion
-  return reverseString(string.substr(1)) + string[0];
+  //return reverseString(string.substr(1)) + string[0];
 
+    //using loop
+    let reversedString = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+      reversedString += string[i];
+    }
+    return reversedString;    
 };
 
 // Do not edit below this line
