4b68143a0e95455ca82648a503cf61ca666f3f86
AU <urrutia.alvaro@gmail.com>
2022-07-28 08:30:08 -0500

    Return if a year is leap or not

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = (year) => {
+
+    //leap years are divisible by 4, 
+    //unless they are divisible by 100, 
+    //but not divisible by 400    
+    if (year % 100 === 0) {
+        return year % 400 === 0;
+    } else {
+        return year % 4 === 0;
+    }
 
 };
 
