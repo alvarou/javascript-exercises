c814283e38221cdee5b39ac864ad61e4b6dfd075
AU <urrutia.alvaro@gmail.com>
2022-07-28 15:05:30 -0500

    Add a function to check if a word is palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+//arrow function to check if a string is a palindrome
+const palindromes = (str) => {
 
-};
+    //remove punctuation characters 
+    str = str.replace(/[^\w\s]/gi, '').toLowerCase();
+    
+    //remove spaces
+    str = str.replace(/\s/g, '');
+
+    //remove commas
+    str = str.replace(/,/g, '');
+
+    return str.split('').reverse().join('') === str;
+
+}
+;
 
 // Do not edit below this line
 module.exports = palindromes;
