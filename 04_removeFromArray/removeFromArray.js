81eb81286df36ceb2a9c33a69bf2c23725172874
AU <urrutia.alvaro@gmail.com>
2022-07-28 07:26:24 -0500

    Remove multiple values from an array
    - Use spread operator to receive multiple optional parameters.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+//use multiple optional arguments to remove multiple values
+//use the spread operator to remove multiple values
+//use the splice method to remove multiple values
+const removeFromArray = function(arr, ...args) {
+    //iterate through the arguments
+    for (let i = 0; i < args.length; i++) {
+        //find the index of the argument
+        let index = arr.indexOf(args[i]);
 
-};
+        //if the index is not -1, remove the value
+        if (index > -1) {
+        arr.splice(index, 1);
+        }
+  }
+  return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
