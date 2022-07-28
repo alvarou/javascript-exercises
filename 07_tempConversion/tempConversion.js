339a4f23d6f3115960463d87611363259cb24036
AU <urrutia.alvaro@gmail.com>
2022-07-28 13:06:32 -0500

    Convert temperatures C to F and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,28 @@
-const ftoc = function() {
-
+//converts fahrenheit to celsius
+//rounded to one decimal 
+const ftoc = (fahrenheit) => {
+  
+  //get the celsius value by subtracting 32 from the fahrenheit value
+  //multiply by 5/9  
+  let result = ((fahrenheit - 32) * 5 / 9);
+  
+  //round result to one decimal
+  result = Math.round(result * 10) / 10;
+  return result;
+  
 };
 
-const ctof = function() {
+//converts celsius to fahrenheit
+//rounded to one decimal
+const ctof = (celsius) => {
+
+  //get the fahrenheit value by multiplying the celsius value by 9/5
+  //add 32 to the result
+  let result = (celsius * 9 / 5) + 32;
+
+  //round result to one decimal
+  result = Math.round(result * 10) / 10;
+  return result;
 
 };
 
